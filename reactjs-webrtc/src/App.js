460640c5e9c9dd50d48a3dd64bae8be9c802456d
thanhlong98 <ntl2898js@gmail.com>
2020-10-14 14:12:38 +0700

    asddasd

---
+++ reactjs-webrtc/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css'
+import CreateRoom from './pages/createRoom'
+import Room from './pages/room'
+import Room2 from './pages/room2'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={CreateRoom} />
+        <Route path="/room/:roomID" component={Room} />
+        <Route path="/room2/:roomID" component={Room2} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
